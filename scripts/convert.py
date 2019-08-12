953a0185cd680d4316f16762427dae5f7764c6a2
krshrimali <kushashwaravishrimali@gmail.com>
2019-08-12 15:21:39 +0530

    Converting model to scripting model without last layer

--- scripts/convert.py
+++ scripts/convert.py
@@ -7,7 +7,9 @@ import torchvision.models as models
 
 resnet18 = models.resnet18(pretrained=True)
 
+print(list(resnet18.children())[-1])
+resnet18 = torch.nn.Sequential(*list(resnet18.children())[:-1]) # Take all layers except the last one
 example_input = torch.rand(1, 3, 224, 224)
 
 script_module = torch.jit.trace(resnet18, example_input)
-script_module.save('resnet18.pt')
+script_module.save('resnet18_without_lastlayer.pt')
