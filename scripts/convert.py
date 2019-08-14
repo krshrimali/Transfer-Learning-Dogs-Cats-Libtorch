4e880bb787938f9dad905d809c4474a6a97ab9ec
krshrimali <kushashwaravishrimali@gmail.com>
2019-08-14 17:25:12 +0530

    Python Equivalent Code for Transfer Learning

--- scripts/convert.py
+++ scripts/convert.py
@@ -6,10 +6,23 @@ import torch
 import torchvision.models as models
 
 resnet18 = models.resnet18(pretrained=True)
+for param in resnet18.parameters():
+	param.requires_grad = False
+resnet18.fc = torch.nn.Linear(512, 2)
+for param in resnet18.fc.parameters():
+	param.requires_grad = True
 
-print(list(resnet18.children())[-1])
-resnet18 = torch.nn.Sequential(*list(resnet18.children())[:-1]) # Take all layers except the last one
+example_input = torch.rand(1, 3, 224, 224)
+script_module = torch.jit.trace(resnet18, example_input)
+script_module.save('resnet18_with_last_layer.pt')
+
+# print(list(resnet18.children()))
+# resnet18 = torch.nn.Sequential(*list(resnet18.children())) # Take all layers except the last one
+# list(resnet18.children())[-1] = torch.nn.Linear(512, 2)
+# print(list(resnet18.children())[-1])
+'''
 example_input = torch.rand(1, 3, 224, 224)
 
 script_module = torch.jit.trace(resnet18, example_input)
 script_module.save('resnet18_without_lastlayer.pt')
+'''
